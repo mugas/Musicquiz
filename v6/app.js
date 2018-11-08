a10ca46e73a1b0cc2243d4af94625110f6bd7fef
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2018-11-08 23:42:59 +0200

    starting debung the code

--- v6/app.js
+++ v6/app.js
@@ -1,3 +1,80 @@
+function Quiz(questions) {
+  this.score = 0;
+  this.questions = questions;
+  this.QuestionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function() {
+  return this.questions[this.QuestionIndex];
+};
+
+Quiz.prototype.isEnded = function() {
+  return this.questions.length === this.QuestionIndex;
+};
+
+Quiz.prototype.guess = function(answer) {
+  if (this.getQuestionIndex().correctAnswer(answer)) {
+    this.score++;
+  }
+  this.QuestionIndex++;
+};
+
+function Question(text, choices, answer) {
+  this.text = text;
+  this.choices = choices;
+  this.answer = answer;
+}
+
+Question.prototype.correctAnswer = function(choice) {
+  return choice === this.answer;
+};
+
+function populate() {
+  if (quiz.isEnded()) {
+    showScores();
+  } else {
+    //show question
+    var element = document.getElementById("question");
+    element.innerHTML = quiz.getQuestionIndex().text;
+
+    //show possible choices
+    var choices = quiz.getQuestionIndex().choices;
+    for (var i = 0; i < choices.length; i++) {
+      var element = document.getElementById("choice" + i);
+      element.innerHTML = choices[i];
+      guess("btn" + i, choices[i]);
+    }
+    showProgress();
+  }
+}
+
+function guess(id, guess) {
+  var button = document.getElementById(id);
+  button.onclick = function() {
+    quiz.guess(guess);
+    populate();
+  };
+}
+
+function showProgress() {
+  var currentQuestionNumber = quiz.QuestionIndex + 1;
+  var element = document.getElementById("progress");
+  element.innerHTML =
+    "Question" + currentQuestionNumber + " of " + quiz.questions.length;
+}
+
+function showScores() {
+  var gameOverHtml = "<h1>Result</h1>";
+  gameOverHtml +=
+    "<h2 id='score' style='margin-top:130px;'> Your scores: " +
+    quiz.score +
+    " out of " +
+    quiz.questions.length +
+    "</h2>";
+  var element = document.getElementById("stripe");
+  element.innerHTML = gameOverHtml;
+}
+
 var questions = [
   new Question(
     "Now the drugs don´t work",
@@ -60,81 +137,6 @@ var questions = [
     "Toto"
   )
 ];
-
-function Quiz(questions) {
-  this.score = 0;
-  this.questions = questions;
-  this.QuestionIndex = 0;
-}
-
-Quiz.prototype.getQuestionIndex = function() {
-  return this.questions[this.QuestionIndex];
-};
-
-Quiz.prototype.isEnded = function() {
-  return (this.questions.length = this.QuestionIndex);
-};
-
-Quiz.prototype.guess = function(answer) {
-  if (this.getQuestionIndex().correctAnswer(answer)) {
-    this.score++;
-  }
-};
-
-function Question(text, choices, answer) {
-  this.text = text;
-  this.choices = choices;
-  this.answer = answer;
-}
-
-Question.prototype.correctAnswer = function(choice) {
-  return choice === this.answer;
-};
-
-function populate() {
-  if (quiz.isEnded()) {
-    showScores();
-  } else {
-    //show question
-    var element = document.getElementById("question");
-    element.innerHTML = quiz.getQuestionIndex().text;
-
-    //show possible choices
-    var choices = Quiz.getQuestionIndex().choices;
-    for (var i = 0; i < choices.length; i++) {
-      var element = document.getElementById("possibleAnswers" + i);
-      element.innerHTML = choices[i];
-      guess("btn" + i, choices[i]);
-    }
-    showProgress();
-  }
-}
-
-function guess(id, guess) {
-  var button = document.getElementById(id);
-  button.onclick = function() {
-    quiz.guess(guess);
-    populate();
-  };
-}
-
-function showProgress() {
-  var currentQuestionNumber = quiz.QuestionIndex + 1;
-  var element = document.getElementById("progress");
-  element.innerHTML =
-    "Question" + currentQuestionNumber + " of " + quiz.questions.length;
-}
-
-function showScores() {
-  var gameOverHtml = "<h1>Result</h1>";
-  gameOverHtml +=
-    "<h2 id='score' style='margin-top:130px;'> Your scores: " +
-    quiz.score +
-    " out of " +
-    quiz.questions.length +
-    "</h2>";
-  element.innerHTML = gameOverHtml;
-}
 
 var quiz = new Quiz(questions);
 populate();
