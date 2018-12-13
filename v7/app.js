3e8decfb13343262cab0f3aa276431b6a75a432c
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2018-12-13 23:14:29 +0200

    add random colors to the buttons

--- v7/app.js
+++ v7/app.js
@@ -154,11 +154,45 @@ function restart() {
 agora.addEventListener("click", restart);
 
 //Random Color
-/* 
-var buttonss = document.querySelectorAll(".buttons");
 
-buttonss.forEach(function(color) {
-  color.= "red";
+var colors = generateRandomColors(6);
+var buttons = document.querySelectorAll(".button");
+//var pickedColor = pickColor();
+/* 
+ buttons.forEach(function(color) {
+  buttons[i].style.backgroundColor = colors[i];
   console.log(color);
-});
- */
+});  */
+ 
+ for(var i = 0;i < buttons.length; i++){
+  
+  buttons[i].style.backgroundColor = colors[i];
+}   
+
+
+
+function pickColor(){
+ var random  = Math.floor(Math.random() * colors.length)
+ return colors[random];
+} 
+
+function generateRandomColors(num){
+  //make an array
+  var arr=[];
+  //repeat num times
+  for (var i = 0; i < num; i++){
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor(){
+  //pick a red from 0 -255
+  var r =Math.floor(Math.random() * 256);
+  //pick a green from 0-255
+  var g =Math.floor(Math.random() * 256);
+  //pick a blue from 0-255
+  var b =Math.floor(Math.random() * 256);
+  return "rgb(" + r + "," + g + "," + b + ")";
+
+}
